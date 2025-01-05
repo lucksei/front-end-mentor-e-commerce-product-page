14c6daf762b6f9d9e98a60a6a7231b3c8dc35486
Luca Seimandi <lseimandi@colsecor.com.ar>
2025-01-05 19:17:15 -0300

    refactored orange button into its own utility class and added styles to the cart modal

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -25,19 +25,25 @@ function Header() {
   };
 
   return (
-    <header className="header">
-      <div className="header--left">
-        <Sidebar open={sidebarOpen} onSidebarOpen={handleSidebarOpen}></Sidebar>
-        <SidebarButton onSidebarOpen={handleSidebarOpen}></SidebarButton>
-        <h1>
-          <Logo></Logo>
-        </h1>
-      </div>
-      <div className="header--right">
-        <CartButton quantity={3}></CartButton>
-        <ProfileButton></ProfileButton>
-      </div>
-    </header>
+    <>
+      <header className="header">
+        <div className="header--left">
+          <Sidebar
+            open={sidebarOpen}
+            onSidebarOpen={handleSidebarOpen}
+          ></Sidebar>
+          <SidebarButton onSidebarOpen={handleSidebarOpen}></SidebarButton>
+          <h1>
+            <Logo></Logo>
+          </h1>
+        </div>
+        <div className="header--right">
+          <CartButton quantity={3}></CartButton>
+          <ProfileButton></ProfileButton>
+        </div>
+      </header>
+      <CartModal></CartModal>
+    </>
   );
 }
 
@@ -186,6 +192,52 @@ function ProfileButton() {
     <button className="header--profile">
       <img src={require("./../../images/image-avatar.png")} alt="profile"></img>
     </button>
+  );
+}
+
+function CartModal() {
+  return (
+    <>
+      <div className="cart-modal">
+        <div className="cart-modal--header">
+          <h3>Cart</h3>
+        </div>
+        <div className="cart-modal--body">
+          <div className="cart-modal--items">
+            {/* <CartModalItem></CartModalItem> */}
+          </div>
+          <button type="button" className="cart-modal--checkout-button">
+            Checkout
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+function CartModalItem() {
+  const itemImage = require("./../../images/image-product-1.jpg");
+
+  const deleteIcon = (
+    <svg width="14" height="16" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M0 2.625V1.75C0 1.334.334 1 .75 1h3.5l.294-.584A.741.741 0 0 1 5.213 0h3.571a.75.75 0 0 1 .672.416L9.75 1h3.5c.416 0 .75.334.75.75v.875a.376.376 0 0 1-.375.375H.375A.376.376 0 0 1 0 2.625Zm13 1.75V14.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 1 14.5V4.375C1 4.169 1.169 4 1.375 4h11.25c.206 0 .375.169.375.375ZM4.5 6.5c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Zm3 0c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Zm3 0c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Z"
+        fill="#C3CAD9"
+        fillRule="nonzero"
+      />
+    </svg>
+  );
+  return (
+    <div className="cart-modal--cart-item">
+      <img className="cart-item--image" src={itemImage} alt="item"></img>
+      <span className="cart-item--name">Fall Limited Edition Sneakers</span>
+      <div className="cart-item--values">
+        <span className="cart-item--price-unit">$125.00</span>
+        <span className="cart-item--quantity">x3</span>
+        <span className="cart-item--price-total">$375.00</span>
+      </div>
+      <button className="cart-item--delete-button">{deleteIcon}</button>
+    </div>
   );
 }
 
